b776099273370e6410f713d5be75a0a26a6ea896
Roisin Hayes Owens <roisinhayesowens@gmail.com>
2019-06-19 21:38:12 -0500

    Spotify Function

--- liri.js
+++ liri.js
@@ -1,18 +1,34 @@
+// REQUIRE THE .env file
 require("dotenv").config();
-
-
+// REQUIRE FILE SYSTEM
+var fs = require('fs')
+// LINKED KEY PAGE
 var keys = require("./keys.js");
 
-var fs = require('fs')
+var request = require('fs')
+var request = require('request');
 
+//REQUIRE spotify api/and link
 var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
 
-spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+var getMeSpotify = function (songName) {
+
+  // Spotify search function
+  spotify.search({ type: 'track', query: 'All the Small Things' },
+    function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+        return;
+      }
+
+      console.log(data.tracks.items[0]);
+    });
+}
+  // for loop in Spotify
+  // let spotifyArr = data.tracks.items;
+
+  // for (var i = 0; i < spotifyArr.length; i++) {
+
+  // };
